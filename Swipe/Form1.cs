416e87285eb51979b3da11ca34f633d2a03ee35a
JDEVLab99 <71048040+JDEVLab99@users.noreply.github.com>
2021-03-07 20:14:23 +0100

    Add files via upload

---
+++ Swipe/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Swipe
+{
+    public partial class Form1 : Form
+    {
+        Swipe word = new Swipe();
+        int val;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "q";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "w";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "e";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "r";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "t";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "y";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "u";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "i";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "o";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "p";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "a";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "s";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "d";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "f";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "g";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "h";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "j";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "k";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "l";
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "z";
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "x";
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "c";
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "v";
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "b";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "n";
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "m";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            word.apertura_file();
+
+            val = word.ricerca(textBox4.Text);
+            if (val == 1)
+            {
+                textBox3.Text = " ";
+                textBox4.Text = word.get_parola();
+                textBox5.Text = " ";
+            }
+            else
+                textBox4.Text = "NOT FOUND";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            word.set_parola(textBox1.Text);
+            word.apertura_file();
+            word.ris();
+            textBox3.Text = word.get_risultato1();
+            textBox4.Text = word.get_risultato2();
+            textBox5.Text = word.get_risultato3();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            word.clear();
+        }
+
+       
+
+
+    }
+    
+}
---
+++ Swipe/Swipe.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace Swipe
+{
+    class Swipe
+    {
+        private string parola_inserire ;
+        private bool trovato = false;
+        private string[] parole = new string [10000];
+        private string[] risultati = new string[3];
+
+        public void set_parola(string parola)
+        {
+            parola_inserire = parola;
+        }
+
+        public string get_parola()
+        {
+            return parola_inserire;
+        }
+
+        public void apertura_file()
+        {
+            if(parola_inserire!=""){
+                switch (parola_inserire[0])
+                {
+                    case 'a':
+                        parole = File.ReadAllLines(@"a.txt");
+                        break;
+
+                    case 'b':
+                        parole = File.ReadAllLines(@"b.txt");
+                        break;
+
+                    case 'c':
+                        parole = File.ReadAllLines(@"c.txt");
+                        break;
+
+                    case 'd':
+                        parole = File.ReadAllLines(@"d.txt");
+                        break;
+
+                    case 'e':
+                        parole = File.ReadAllLines(@"e.txt");
+                        break;
+
+                    case 'f':
+                        parole = File.ReadAllLines(@"f.txt");
+                        break;
+
+                    case 'g':
+                        parole = File.ReadAllLines(@"g.txt");
+                        break;
+
+                    case 'h':
+                        parole = File.ReadAllLines(@"h.txt");
+                        break;
+
+                    case 'i':
+                        parole = File.ReadAllLines(@"i.txt");
+                        break;
+
+                    case 'l':
+                        parole = File.ReadAllLines(@"l.txt");
+                        break;
+
+                    case 'm':
+                        parole = File.ReadAllLines(@"m.txt");
+                        break;
+
+                    case 'n':
+                        parole = File.ReadAllLines(@"n.txt");
+                        break;
+
+                    case 'o':
+                        parole = File.ReadAllLines(@"o.txt");
+                        break;
+
+                    case 'p':
+                        parole = File.ReadAllLines(@"p.txt");
+                        break;
+
+                    case 'q':
+                        parole = File.ReadAllLines(@"q.txt");
+                        break;
+
+                    case 'r':
+                        parole = File.ReadAllLines(@"r.txt");
+                        break;
+
+                    case 's':
+                        parole = File.ReadAllLines(@"s.txt");
+                        break;
+
+                    case 't':
+                        parole = File.ReadAllLines(@"t.txt");
+                        break;
+
+                    case 'u':
+                        parole = File.ReadAllLines(@"u.txt");
+                        break;
+
+                    case 'v':
+                        parole = File.ReadAllLines(@"v.txt");
+                        break;
+
+                    case 'w':
+                        parole = File.ReadAllLines(@"w.txt");
+                        break;
+
+                    case 'x':
+                        parole = File.ReadAllLines(@"x.txt");
+                        break;
+
+                    case 'z':
+                        parole = File.ReadAllLines(@"z.txt");
+                        break;
+                    default:
+                        {
+                        }; break;
+                }
+            };
+        }
+
+        public int ricerca(string parola)
+        {
+            int i=0;
+            trovato = false;
+            do
+            {
+                if (parola_inserire == parole[i])
+                    trovato = true;
+                else
+                    i++;
+            } while (trovato==false && i < parole.Length);
+
+            if (trovato == true)
+                return 1;
+            else
+                return -1;    
+        }
+
+        public void ris()
+        {
+            int i = 0;
+            int j = 0;
+            int temp=0;
+
+            //CONTROLLO LE PROLE CHE NON DEVO INSERIRE
+            if (parola_inserire == "" || parola_inserire.ElementAt(0) == 'j' || parola_inserire.ElementAt(0) == 'k' || parola_inserire.ElementAt(0) == 'y')
+                set_all("NOT FOUND");
+            
+            else
+            {
+                //ENTRA NEL CICLO E LEGGO LE PAROLE DI TUTTO IL FILE 
+                do
+                {
+                    //SE IL NUMERO DELLE PAROLE E' SUPERIORE A 3
+                    if (parole.Length > 3)
+                    {
+                        //CONTROLLO SULLA LUNGHEZZA DELLE DUE PAROLE 
+                        if (parola_inserire.Length == parole.ElementAt(i).Length || parola_inserire.Length + 2 == parole.ElementAt(i).Length )
+                        {
+                            temp=parola_inserire.Length;
+
+                            if (parola_inserire[temp - 1] == parole[i][temp - 1])
+                                risultati[j] = parole[i];
+                            j++;
+                        }
+                        else
+                        {
+                            set_r1(risultati.ElementAt(0));
+                            set_r2(risultati.ElementAt(1));
+                            set_r3(risultati.ElementAt(2));
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        if (parole.Length == 3)
+                        {
+                            set_r1(parole.ElementAt(0));
+                            set_r2(parole.ElementAt(1));
+                            set_r3(parole.ElementAt(2));
+                        }
+                        else if (parole.Length == 2)
+                        {
+                            set_r1(parole.ElementAt(0));
+                            set_r2(parole.ElementAt(1));
+                            set_r3("");
+                        }
+                        else if (parole.Length == 1)
+                        {
+                            set_r1(parole.ElementAt(0));
+                            set_r2("");
+                            set_r3("");
+                        }
+                        else
+                        {
+                            set_all("");
+                        }
+                        j = 3;
+                    }
+
+                } while (i < (parole.Length - 1) && j != 3 );
+            }
+        }
+
+        private void set_all(string t)
+        {
+            set_r1(t);
+            set_r2(t);
+            set_r3(t);
+        }
+
+        private void set_r1(string r1)
+        {
+            risultati[0] = r1;
+        }
+
+        private void set_r2(string r1)
+        {
+            risultati[1] = r1;
+        }
+
+        private void set_r3(string r1)
+        {
+            risultati[2] = r1;
+        }
+
+        
+        public string get_risultato1()
+        {
+            return risultati[0];
+        }
+
+        public string get_risultato2()
+        {
+            return risultati[1];
+        }
+
+        public string get_risultato3()
+        {
+            return risultati[2];
+        }
+
+        
+        public void clear()
+        {
+            parola_inserire="";
+            Array.Clear(risultati, 0, risultati.Length);
+        }
+         
+    }
+}
